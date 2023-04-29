a6c583483f6e21c50c5a08304c6bbc643366bead
Ruturaj Bayad <109090403+ruturajbayad@users.noreply.github.com>
2023-04-29 17:18:38 +0530

    Add files via upload

---
+++ src/header/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./navbar.css";
+// import { GiHamburgerMenu } from "react-icons/gi";
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/header/header.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+const header = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default header;
